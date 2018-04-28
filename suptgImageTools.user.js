5c2d140bac4a08071637dcc01b47581faa90581d
Colin O'Neill <colin@faokryn.com>
2018-04-28 17:33:55 -0400

    Version 1.0.1
    Refactored to create a new scope in the loop using an IIFE so that
    moving the HTMLCollection into an Array is unnecessary.

--- suptgImageTools.user.js
+++ suptgImageTools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         SupTG Image Tools
-// @version      1.0
+// @version      1.0.1
 // @namespace    https://github.com/faokryn/suptg-image-tools
 // @description  Expands all images in a SupTG thread and makes clicking an image toggle size
 // @author       Colin O'Neill <colin@faokryn.com>
@@ -25,34 +25,25 @@
         }
     }
 
-    var i, img, imgArr, images;
+    var images = document.getElementsByName("delform")[0].getElementsByTagName("img");
 
-    images = document.getElementsByName("delform")[0].getElementsByTagName("img");
+    for (let i = 0; i < images.length; i++) {
+        (function () {
+            var img = images[i];
 
-    // move the img elements from their HTMLCollection to an Array
-    // this is done to give access to the forEach method. Using a while loop and getting the img element via image[i]
-    // does not work, as it causes the event on every link to resize the last image
-    // this may be because no new scope is created by a while loop, so the same memory is used for the img variable
-    // and simply overwritten, so the function in the event is called on it's most recent contents.
-    imgArr = [];
-    i = 0;
-    while (i < images.length) {
-        imgArr.push(images[i++]);
-    }
+            // remove width and height constraints on image
+            img.setAttribute("width", "");
+            img.setAttribute("height", "");
 
-    imgArr.forEach(function (img, i) {
-        // remove width and height constraints on image
-        img.setAttribute("width", "");
-        img.setAttribute("height", "");
-
-        // default to full size image instead of thumbnail
-        toggleImgSize(img);
-
-        // add event to image click to toggle image size
-        img.parentElement.addEventListener("click", function (evt) {
-            evt.preventDefault();
+            // default to full size image instead of thumbnail
             toggleImgSize(img);
-        });
-    });
+
+            // add event to image click to toggle image size
+            img.parentElement.addEventListener("click", function (evt) {
+                evt.preventDefault();
+                toggleImgSize(img);
+            });
+        })();
+    }
 
 })();
